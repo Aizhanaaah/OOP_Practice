7b0c001a8251b879eafaf802f224b1f41e999c38
Aizhan Amanova <131990972+Aizhanaaah@users.noreply.github.com>
2025-11-20 00:42:39 +0100

    Add files via upload

---
+++ Zoo/Care.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    internal class Care
+    {
+        string animal, careType;
+        DateTime careDate;
+        public string Animal { get { return animal; } set { animal = value; } }
+        public string CareType { get { return careType; } set {careType = value; } }
+        public DateTime CareDate 
+        { 
+            get { return careDate; } 
+            set { if(careDate.Year < 2025) 
+                careDate = value; }
+        }
+        public Care(string animal, string careType, DateTime careDate)
+        {
+            Animal = animal;
+            CareType = careType;
+            CareDate = careDate;
+        }
+        string showInfo()
+        {
+            return $"Animal: {Animal}, Care Type: {CareType}, Care Date: {CareDate.ToShortDateString()}";
+        }
+    }
+}
