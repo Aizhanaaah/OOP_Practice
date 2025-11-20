1cffb7ee32429cbe9f61474f1ee1cfc6245fbf26
Aizhan Amanova <131990972+Aizhanaaah@users.noreply.github.com>
2025-11-20 02:48:05 +0100

    Refactor Predator class to implement ICareTaking

--- Zoo/Predator.cs
+++ Zoo/Predator.cs
@@ -1,13 +1,41 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Zoo : Animal
+namespace Zoo 
 {
-    internal class Predator
+    internal class Predator : Animal, ICareTaking
     {
-    string feedType;
+        public string feedType { get; set; }
+        public string FeedType 
+        { 
+            get { return feedType; } 
+            set 
+            { 
+                if(value == "Meat" || value == "Fish" || value == "Poultry") 
+                    feedType = value; 
+            }
+        }
+        public Predator(string name, string species, string caretaker, DateTime birthDate, string feedType)
+            : base(name, species, caretaker, birthDate)
+        {
+            FeedType = feedType;
+        }
 
+        public override void AddCare()
+        {
+            Console.WriteLine($"Predator-specific care added for {Name}.");
+        }
+
+        public override void RemoveCare()
+        {
+            Console.WriteLine($"Predator-specific care removed for {Name}.");
+        }
+        public override string showInfo()
+        {
+            return base.showInfo() + $", Feed Type: {FeedType}";
+        }
+    }
 }
